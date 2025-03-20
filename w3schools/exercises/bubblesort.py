4f985a8cfefc6c175deafc335182f25398a4f371
Kigula Jesse <jkigula@icloud.com>
2025-03-20 14:15:27 +0300

    Real Life Bubble Sort

--- w3schools/exercises/bubblesort.py
+++ w3schools/exercises/bubblesort.py
@@ -45,3 +45,61 @@ def bubble_left_ascending(array):
 
 test_array = [5, 3, 8, 4, 2]
 print("Bubble Left Ascending:",bubble_left_ascending(test_array))
+
+def count_swaps_bubble_sort(array):
+    "Count swaps in Bubble Sort"
+    size = len(array)
+    swaps = 0
+    for i in range(size-1):
+        for j in range(size-1,i,-1):
+            if array[j] < array[j-1]:
+                swaps += 1
+                array[j], array[j-1] = array[j-1],array[j]
+    return array,swaps
+
+test_array = [5, 3, 8, 4, 2]
+print("Count Swaps Bubble Sort: ", "array,swaps = ", count_swaps_bubble_sort(test_array))  
+
+def optimized_bubble(array):
+    size = len(array)
+    swaps = 0
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1,i,-1):
+            if array[j] < array[j-1]:
+                swapped = True
+                swaps += 1
+                array[j], array[j-1] =  array[j-1], array[j]
+        if not swapped:
+            "Since there are no swaps, list is sorted no need to continue"
+            break
+    return array,swaps
+
+test_array = [1, 2, 3, 4, 5]
+print("Count Swaps Bubble Sort: ", "array,swaps = ", optimized_bubble(test_array))
+
+def bubble_sort_on_strings(array):
+    """Bubble sort on strings"""
+    size = len(array)
+    for i in range(size-1):
+        for j in range(size-1,i,-1):
+            if array[j] < array[j-1]:
+                array[j], array[j-1] = array[j-1],array[j]
+    return array
+
+test_array = ["apple", "appld", "app"]
+print(bubble_sort_on_strings(test_array))
+
+def bubblesortbycustom(array):
+    """Bubble sort based on key"""
+    size = len(array)
+    for i in range(size-1):
+        for j in range(size-i-1):
+            "access second element in tuple"
+            if array[j][1] < array[j+1][1]:
+                array[j],array[j+1] = array[j+1],array[j]
+    return array
+
+test_tuple_array = [("a", 3), ("b", 1), ("c", 2)]
+print("==========================")
+print(bubblesortbycustom(test_tuple_array))
--- w3schools/exercises/test.py
+++ w3schools/exercises/test.py
@@ -1,160 +1,179 @@
-# #             0, 1, 2, 3, 4, 5, 6, 7, 8
-# test_array = [1, 7, 3, 0, 5, 4, 7, 8, 9]
 
-# print(test_array[:0:])
-# print(test_array[:1:])
-# print(test_array[:2:])
-# print(test_array[:3:])
-# print(test_array[:4:])
-# print(test_array[:5:])
-# print(test_array[:6:])
-# print(test_array[:7:])
+
+# def compare(a, b):
+#     print(a)
+#     print(b)
+#     print(a, "<", b, " = ", a < b)
+#     print(a, ">", b, " = ", a > b)
+#     print("int(True) = ",int(True))
+#     print("int(False) = ",int(False))
+#     print("True - False =", True - False)
+#     print("False - True =", False - True)
+#     return ((a < b) - (a > b))
+
+# compare("apple", "appld")
+
+test_tuple = (3,4,5)
+
+print(test_tuple[1])
+
+# # #             0, 1, 2, 3, 4, 5, 6, 7, 8
+# # test_array = [1, 7, 3, 0, 5, 4, 7, 8, 9]
+
+# # print(test_array[:0:])
+# # print(test_array[:1:])
+# # print(test_array[:2:])
+# # print(test_array[:3:])
+# # print(test_array[:4:])
+# # print(test_array[:5:])
+# # print(test_array[:6:])
+# # print(test_array[:7:])
+# # print("")
+
+# # print(len(test_array))
+# # print(range(len(test_array)))
+
+# # ####--- range just creates iterators over a range, so it allows us to go beyond the array size
+# # for i in range(len(test_array) + 1):
+# #     # print(i)
+# #     print(test_array[:i:])
+
+# # print("")
+
+# # ####--- array slicing stops exactly at the last index even if you add +1. 
+# # # Since the last index is exclusive, it will not print the last item
+# # for i,_ in enumerate(test_array[:len(test_array)+1:]):
+# #     # print(i)
+# #     print(test_array[:i:])
+# # print(test_array[:i+1:])
+
+# # print("")
+# # print(test_array[:len(test_array):])
+# # print(test_array[:len(test_array)+1:])
+
+
+# #              0,1,2,3,4,5
+# other_array = [5,3,9,2,8,7]
+# # print(other_array[5:])
+# # print(other_array[4:])
+# # print(other_array[3:])
+# # print(other_array[2:])
+# # print(other_array[1:])
+# # print(other_array[0:])
+
+# size = len(other_array)
+
+# print(range(size))
+
+# for j in range(size):
+#     print(j," : ",other_array[j])
+
 # print("")
 
-# print(len(test_array))
-# print(range(len(test_array)))
-
-# ####--- range just creates iterators over a range, so it allows us to go beyond the array size
-# for i in range(len(test_array) + 1):
-#     # print(i)
-#     print(test_array[:i:])
-
-# print("")
-
-# ####--- array slicing stops exactly at the last index even if you add +1. 
-# # Since the last index is exclusive, it will not print the last item
-# for i,_ in enumerate(test_array[:len(test_array)+1:]):
-#     # print(i)
-#     print(test_array[:i:])
-# print(test_array[:i+1:])
-
-# print("")
-# print(test_array[:len(test_array):])
-# print(test_array[:len(test_array)+1:])
-
-
-#              0,1,2,3,4,5
-other_array = [5,3,9,2,8,7]
-# print(other_array[5:])
-# print(other_array[4:])
-# print(other_array[3:])
-# print(other_array[2:])
-# print(other_array[1:])
-# print(other_array[0:])
-
-size = len(other_array)
-
-print(range(size))
-
-for j in range(size):
-    print(j," : ",other_array[j])
-
-print("")
-
-for j in range(size-1, -1, -1):
-    print(j," : ",other_array[j])
+# for j in range(size-1, -1, -1):
+#     print(j," : ",other_array[j])
     
 
-"Bubble Sort Exercises from ChatGPT"
+# "Bubble Sort Exercises from ChatGPT"
 
-def basic_bubblesort(array):
-    "This Bubble Sort Function organises array in Ascending Order"
-    size = len(array)
-    # print(range(size-1))
-    for i,_ in enumerate(array[:size:]):
-        for j,_ in enumerate(array[:size-i-1:]):
-            print(array[:size-i:])
-            print(j)
-            print(j + 1)
-            print("")
-            if array[j] > array[j+1]:
-                array[j],array[j+1] = array[j+1],array[j]
-    return array
-
-# test_array = [5, 3, 8, 4, 2]
-# print(basic_bubblesort(test_array))
-
-# def descending_bubble(array):
-#     "This is descending order Bubble Sort"
+# def basic_bubblesort(array):
+#     "This Bubble Sort Function organises array in Ascending Order"
 #     size = len(array)
-#     for i in range(size-1):
-#         for j in range(size-i-1):
-#             if array[j] < array[j+1]:
+#     # print(range(size-1))
+#     for i,_ in enumerate(array[:size:]):
+#         for j,_ in enumerate(array[:size-i-1:]):
+#             print(array[:size-i:])
+#             print(j)
+#             print(j + 1)
+#             print("")
+#             if array[j] > array[j+1]:
 #                 array[j],array[j+1] = array[j+1],array[j]
 #     return array
 
-# test_array = [5, 3, 8, 4, 2]
-# print(descending_bubble(test_array))
+# # test_array = [5, 3, 8, 4, 2]
+# # print(basic_bubblesort(test_array))
 
-def descending_bubble_alternate(array):
-    "This is descending order Bubble Sort"
-    size = len(array)
-    for i in range(size-1):
-        for j in range(size-1, i, -1):
-            print(j, size-i-1)
-            print(array[j],">" ,array[j-1]," = ", array[j] > array[j-1] ,"(",j,j-1,")")
-            if array[j] > array[j-1]:
-                array[j], array[j-1] = array[j-1], array[j]
-        # print(array)
-        print("")
-    return array
+# # def descending_bubble(array):
+# #     "This is descending order Bubble Sort"
+# #     size = len(array)
+# #     for i in range(size-1):
+# #         for j in range(size-i-1):
+# #             if array[j] < array[j+1]:
+# #                 array[j],array[j+1] = array[j+1],array[j]
+# #     return array
 
-test_array = [5, 3, 8, 4, 2]
-print(descending_bubble_alternate(test_array))
-
-def descending_bubble_sort_count_swaps(array):
-    size = len(array)
-    swaps = 0
-    for i in range(size-1):
-        for j in range(size-1, i, -1):
-            print(j, size-i-1)
-            print(array[j],">" ,array[j-1]," = ", array[j] > array[j-1] ,"(",j,j-1,")")
-            if array[j] < array[j-1]:
-                swaps += 1
-                array[j], array[j-1] = array[j-1], array[j]
-        # print(array)
-        print("")
-    return array,swaps
-
-test_array = [5, 3, 8, 4, 2]
-print(descending_bubble_sort_count_swaps(test_array))
-
-
-
-
-
-
-
+# # test_array = [5, 3, 8, 4, 2]
+# # print(descending_bubble(test_array))
 
 # def descending_bubble_alternate(array):
 #     "This is descending order Bubble Sort"
 #     size = len(array)
 #     for i in range(size-1):
-#         print(i)
-#         for j in range(i, size-1, 1):
-#             print(j)
-#         #     print(array[j],">" ,array[j-1]," = ", array[j] > array[j-1] ,"(",j,j-1,")")
-#         #     if array[j] > array[j-1]:
-#         #         array[j], array[j-1] = array[j-1], array[j]
+#         for j in range(size-1, i, -1):
+#             print(j, size-i-1)
+#             print(array[j],">" ,array[j-1]," = ", array[j] > array[j-1] ,"(",j,j-1,")")
+#             if array[j] > array[j-1]:
+#                 array[j], array[j-1] = array[j-1], array[j]
 #         # print(array)
-#         # print("")
-#     # return array
-
-# test_array = [5, 3, 8, 4, 2]
-# print(descending_bubble_alternate(test_array))
-
-
-# def descending_bubble(array):
-#     "This is descending order Bubble Sort"
-#     size = len(array)
-#     for i in range(size-1):
-#         for j in range(size-i-1):
-#             print(j)
-#             # if array[j] < array[j+1]:
-#             #     array[j],array[j+1] = array[j+1],array[j]
 #         print("")
 #     return array
 
 # test_array = [5, 3, 8, 4, 2]
-# print(descending_bubble(test_array))
-# print("================")
+# print(descending_bubble_alternate(test_array))
+
+# def descending_bubble_sort_count_swaps(array):
+#     size = len(array)
+#     swaps = 0
+#     for i in range(size-1):
+#         for j in range(size-1, i, -1):
+#             print(j, size-i-1)
+#             print(array[j],">" ,array[j-1]," = ", array[j] > array[j-1] ,"(",j,j-1,")")
+#             if array[j] < array[j-1]:
+#                 swaps += 1
+#                 array[j], array[j-1] = array[j-1], array[j]
+#         # print(array)
+#         print("")
+#     return array,swaps
+
+# test_array = [5, 3, 8, 4, 2]
+# print(descending_bubble_sort_count_swaps(test_array))
+
+
+
+
+
+
+
+
+# # def descending_bubble_alternate(array):
+# #     "This is descending order Bubble Sort"
+# #     size = len(array)
+# #     for i in range(size-1):
+# #         print(i)
+# #         for j in range(i, size-1, 1):
+# #             print(j)
+# #         #     print(array[j],">" ,array[j-1]," = ", array[j] > array[j-1] ,"(",j,j-1,")")
+# #         #     if array[j] > array[j-1]:
+# #         #         array[j], array[j-1] = array[j-1], array[j]
+# #         # print(array)
+# #         # print("")
+# #     # return array
+
+# # test_array = [5, 3, 8, 4, 2]
+# # print(descending_bubble_alternate(test_array))
+
+
+# # def descending_bubble(array):
+# #     "This is descending order Bubble Sort"
+# #     size = len(array)
+# #     for i in range(size-1):
+# #         for j in range(size-i-1):
+# #             print(j)
+# #             # if array[j] < array[j+1]:
+# #             #     array[j],array[j+1] = array[j+1],array[j]
+# #         print("")
+# #     return array
+
+# # test_array = [5, 3, 8, 4, 2]
+# # print(descending_bubble(test_array))
+# # print("================")
